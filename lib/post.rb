3a5f0bb153507ad5d7e2c470a95cc80235deb3f6
Aziz Kosber <a.kosber@gmail.com>
2018-08-01 03:45:52 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,14 +3,22 @@ class Post
 
   @@all = []
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
-  
+
   def initialize(title)
     @title = title
     @@all << self
   end
 
+  def author_name
+    if self.author 
+      self.author.name
+    else
+      nil
+    end
+  end
+  
 
 end
